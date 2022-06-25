18bcf6a982d8c94da4d95f42ec900cf4afa2968d
MarkelCA <cuestaarribas.markel@proton.me>
2022-06-26 00:13:54 +0200

    added spring project

---
+++ app/src/main/java/com/akita/sys_management/SysManagementApplication.java
@@ -0,0 +1,13 @@
+package com.akita.sys_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysManagementApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/akita/sys_management/SysManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akita.sys_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
